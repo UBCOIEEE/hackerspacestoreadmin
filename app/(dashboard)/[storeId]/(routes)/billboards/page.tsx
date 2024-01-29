ce0f590b662f34afe306cbc514a070c44a49b558
ytulenov <86948036+ytulenov@users.noreply.github.com>
2024-01-29 04:31:50 -0800

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -2,7 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { format } from "date-fns"
 import { BillboardClient } from "./components/client";
 import { BillboardColumn } from "./components/columns";
-
+ 
 const BillboardsPage = async ({
     params
 }:{
