382eb128648a113ef11e0d5f15030f6e4b01322d
Yerkin <chair@ubcoieee.org>
2024-01-30 16:27:37 -0800

    d

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -99,7 +99,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
             ...value,
             price: parseFloat(String(value?.price)),
           }))
-          .sort((a: { index: number; }, b: { index: number; }) => a.index - b.index),
+          .sort((a: any, b:any) => a.index - b.index),
         //values: [...initialData.values].sort((a, b) => a.index - b.index),
       }
     : {
