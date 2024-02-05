8cc348cefa508723bcf994eaac2c1cec3330c137
Yerkin <chair@ubcoieee.org>
2024-02-04 19:15:05 -0800

    ds

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -222,6 +222,14 @@ export async function DELETE (
             return new NextResponse("Unauthorized", {status: 405});
         }
 
+        const productvalue = await prismadb.productvalue.deleteMany({
+            where:{
+                productId: params.productId
+            }
+        })
+
+         NextResponse.json(productvalue)
+
         const product = await prismadb.product.deleteMany({
             where: {
                 id: params.productId,
